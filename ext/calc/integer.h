bf39e0c345662eaab847f8fe2e6b71b93733cf8f
Tim Peters <zomg.tim@gmail.com>
2015-11-03 06:38:55 +1030

    indent

--- ext/calc/integer.h
+++ ext/calc/integer.h
@@ -6,4 +6,4 @@ void define_calc_z();
 /* macro to test if a ruby VALUE is a ZVALUE */
 #define ISZVALUE(v) (TYPE(v) == T_DATA && RDATA(v)->dfree == cz_free)
 
-#endif  /* INTEGER_H */
+#endif                          /* INTEGER_H */
