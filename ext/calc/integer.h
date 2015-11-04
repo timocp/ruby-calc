1fd2eb497dfd4656b5f07398d9f476469f145ecb
Tim Peters <zomg.tim@gmail.com>
2015-11-04 18:26:15 +1030

    tidy - extern for externals

--- ext/calc/integer.h
+++ ext/calc/integer.h
@@ -1,12 +1,12 @@
 #ifndef INTEGER_H
 #define INTEGER_H 1
 
-void define_calc_z();
-ZVALUE value_to_zvalue(VALUE arg);
+extern void define_calc_z();
+extern ZVALUE value_to_zvalue(VALUE arg);
 
 extern const rb_data_type_t calc_z_type;
 
-/* macro to test if a ruby VALUE is a ZVALUE */
+/* macro to test if a ruby VALUE is a Calc::Z object */
 #define ISZVALUE(v) (rb_typeddata_is_kind_of((v), &calc_z_type))
 
 /* shortcut for getting pointer to Calc::Z's ZVALUE */
--- ext/calc/rational.h
+++ ext/calc/rational.h
@@ -1,10 +1,11 @@
 #ifndef RATIONAL_H
 #define RATIONAL_H 1
 
-void define_calc_q();
+extern void define_calc_q();
 
 extern const rb_data_type_t calc_q_type;
 
+/* macro to test if a ruby VALUE is a Calc::Q object */
 #define ISQVALUE(v) (rb_typeddata_is_kind_of((v), &calc_q_type))
 
 #endif
