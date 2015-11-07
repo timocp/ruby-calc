b4a101a343a889117748bbe619574ef4c171edb0
Tim Peters <zomg.tim@gmail.com>
2015-11-07 14:48:57 +1030

    make extconf.rb work if libcustcalc is missing

--- ext/calc/extconf.rb
+++ ext/calc/extconf.rb
@@ -4,13 +4,14 @@ if !find_header("calc/cmath.h")
   abort "calc/cmath.h is missing, please install calc development headers"
 end
 
-# if custcalc is compiled into calc, then you need both.  need to find a way
-# for #have_library or similar to try both first, then libcalc by itself if
-# both fail.
-# meantime, just always use it.
-$libs = append_library($libs, "custcalc")
+# try libcalc by itself first - this will work if libcustcalc wasn't made,
+# but will fail if it was
 if !have_library("calc", "libcalc_call_me_first")
-  abort "can't find libcalc, please install calc"
+  puts "trying again with -lcustcalc"
+  $libs = append_library($libs, "custcalc")
+  if !have_library("calc", "libcalc_call_me_first")
+    abort "can't find libcalc, please install calc"
+  end
 end
 
 create_makefile("calc/calc")
