300b9c7459b591246d712241733d659d665c3e6b
Tim Peters <zomg.tim@gmail.com>
2016-02-05 18:56:24 +1030

    better testing of Calc delegation module methods

--- test/test_calc.rb
+++ test/test_calc.rb
@@ -5,29 +5,39 @@ class TestCalc < Minitest::Test
     refute_nil ::Calc::VERSION
   end
 
-  def check_delegation_value(m, ruby_n, calc_n)
+  def check_delegation_value(m, ruby_n, calc_n, extra_args_count)
     assert_respond_to Calc, m
+    extra_args = [ruby_n] * extra_args_count
     begin
-      expected = calc_n.__send__(m)
+      expected = calc_n.__send__(*([m] + extra_args))
     rescue => e
+      refute_instance_of NoMethodError, e
       expected_exception = e.class
     end
     if expected_exception
-      assert_raises(expected_exception) { Calc.__send__(m, ruby_n) }
+      assert_raises(expected_exception) { Calc.__send__(*([m, ruby_n] + extra_args)) }
     else
-      actual = Calc.__send__(m, ruby_n)
+      actual = Calc.__send__(*([m, ruby_n] + extra_args))
       assert_instance_of expected.class, actual
       assert_equal expected, actual
     end
   end
 
-  def check_delegation(m)
+  def check_real_delegation(m, arg_count = 1)
     [-2, -1, 0, 0.5, 1, 2, "10"].map do |n|
-      check_delegation_value(m, n, Calc::Q(n))
+      check_delegation_value(m, n, Calc::Q(n), arg_count - 1)
     end
+  end
+
+  def check_complex_delegation(m, arg_count = 1)
     [1+1i, 1-1i, 1i, -1i, -1+1i, -1-1i].map do |n|
-      check_delegation_value(m, n, Calc::C(n))
+      check_delegation_value(m, n, Calc::C(n), arg_count - 1)
     end
+  end
+
+  def check_delegation(m, arg_count = 1)
+    check_real_delegation(m, arg_count)
+    check_complex_delegation(m, arg_count)
   end
 
   def test_delegations
@@ -48,7 +58,7 @@ class TestCalc < Minitest::Test
     check_delegation :asin
     check_delegation :asinh
     check_delegation :atan
-    #check_delegation :atan2
+    check_real_delegation :atan2, 2
     check_delegation :atanh
     check_delegation :cos
     check_delegation :cosh
@@ -56,12 +66,12 @@ class TestCalc < Minitest::Test
     check_delegation :coth
     check_delegation :csc
     check_delegation :csch
-    check_delegation :fact
+    check_real_delegation :fact
     check_delegation :gd
     check_delegation :im
     check_delegation :isreal
-    check_delegation :pi
-    #check_delegation :quomod
+    check_real_delegation :pi
+    check_real_delegation :quomod, 2
     check_delegation :re
     check_delegation :sec
     check_delegation :sech
